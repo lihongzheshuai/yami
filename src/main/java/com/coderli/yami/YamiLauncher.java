ebd00e493ef4cbde4b956779e35ee9aa78950cb1
wushikezuo@gmail.com <wushikezuo@gmail.com>
2016-03-02 23:09:36 +0800

    交互式输入信息

--- src/main/java/com/coderli/yami/YamiLauncher.java
+++ src/main/java/com/coderli/yami/YamiLauncher.java
@@ -1,11 +1,14 @@
 package com.coderli.yami;
 
+import com.coderli.yami.common.constant.PostConstants;
 import com.coderli.yami.common.init.YamiInitializer;
 import com.coderli.yami.posts.DefaultPostHandler;
 import com.coderli.yami.posts.PostHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Date;
 
 /**
@@ -21,9 +24,23 @@ public class YamiLauncher {
     public static void main(String[] args) throws Exception {
         YamiInitializer.init();
         logger.info("启动Yami应用.{}", new Date());
-        String fileName = "migrate blog again and new house property";
-        String title = "20160302 博客再次迁移和房产证办理";
-        String[] tags = new String[]{"OneCoder", "日记"};
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = null;
+        while (fileName == null || fileName.isEmpty()) {
+            System.out.println("请输入文件名:");
+            fileName = bufferedReader.readLine();
+        }
+        String title = null;
+        while (title == null || title.isEmpty()) {
+            System.out.println("请输入文章名:");
+            title = bufferedReader.readLine();
+        }
+        String tagLine = null;
+        while (tagLine == null || tagLine.isEmpty()) {
+            System.out.println("请输入tags,以,分割:");
+            tagLine = bufferedReader.readLine();
+        }
+        String[] tags = tagLine.split(",");
         PostHandler postHandler = new DefaultPostHandler();
         postHandler.autoNewPost(fileName, title, tags);
 
