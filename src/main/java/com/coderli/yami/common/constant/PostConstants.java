926e0495a14f08aa24c41ffdb6285525d81be198
wushikezuo@gmail.com <wushikezuo@gmail.com>
2016-03-03 14:25:58 +0800

    时间格式,增加时区

--- src/main/java/com/coderli/yami/common/constant/PostConstants.java
+++ src/main/java/com/coderli/yami/common/constant/PostConstants.java
@@ -18,7 +18,7 @@ public class PostConstants {
     public static final String HEADER_SEPERATOR = "---";
     public static final String HEADER_CONTENT_SEPERATOR = ":";
 
-    public static final String HEADER_DATE_PATTERN = "yyyy-MM-dd hh:mm:ss";
+    public static final String HEADER_DATE_PATTERN = "yyyy-MM-dd HH:mm:ss +0800";
 
     public static final String POST_FILE_EXT = ".md";
 
